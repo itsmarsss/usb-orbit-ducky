b5e763e547647bcf5663cf818aca113bfb4c5fe1
itsmarsss <itsmarzzzzzz@gmail.com>
2023-10-20 18:41:05 -0400

    Add basic webserver

--- orbit-ducky-controller/src/main.cpp
+++ orbit-ducky-controller/src/main.cpp
@@ -2,21 +2,56 @@
 
 #include <Wire.h>
 
-// function that executes whenever data is requested by master
-// this function is registered as an event, see setup()
-void requestEvent()
+#include <ESPAsyncWebServer.h>
+#include <ESPmDNS.h>
+#include <SPIFFS.h>
+
+#include "wifi_credentials.h"
+
+void requestEvent();
+
+AsyncWebServer server(80);
+
+void notFound(AsyncWebServerRequest *request)
 {
-  Wire.write("hello "); // respond with message of 6 bytes
-  // as expected by master
+  request->send(404, "text/plain", "Not found");
 }
+IPAddress IP(192, 168, 1, 1);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
 
 void setup()
 {
-  Wire.begin(8);                // join i2c bus with address #8
-  Wire.onRequest(requestEvent); // register event
+  Serial.begin(115200);
+
+  if (!SPIFFS.begin(true))
+  {
+    Serial.println("Error mounting SPIFFS");
+    return;
+  }
+
+  WiFi.softAP(ssid, password);
+  delay(500);
+  WiFi.softAPConfig(IP, gateway, subnet);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  server.on("/index.html", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/index.html", String(), false); });
+
+  server.begin();
+
+  Wire.begin(8);
+  Wire.onRequest(requestEvent);
 }
 
 void loop()
 {
   delay(100);
+}
+
+void requestEvent()
+{
+  Wire.write("hello ");
 }
---
+++ orbit-ducky-controller/src/wifi_credentials.h
@@ -0,0 +1,2 @@
+const char *ssid = "Wireless Controller";
+const char *password = "12345678";
