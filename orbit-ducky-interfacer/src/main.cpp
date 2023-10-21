d814ccf36b5df7aaca54c85f4b1d4250fe55962d
itsmarsss <itsmarzzzzzz@gmail.com>
2023-10-20 22:22:17 -0400

    Add keyboard controller

--- orbit-ducky-interfacer/src/main.cpp
+++ orbit-ducky-interfacer/src/main.cpp
@@ -3,21 +3,84 @@
 
 #include <Wire.h>
 
+void write(byte decimal);
+void press(byte decimal);
+void release(byte decimal);
+void releaseAll();
+
 void setup()
 {
-  Wire.begin();       // join i2c bus (address optional for master)
-  Serial.begin(9600); // start serial for output
+  Serial.begin(9600);
+  Wire.begin();
+  Keyboard.begin();
 }
 
 void loop()
 {
-  Wire.requestFrom(8, 6); // request 6 bytes from peripheral device #8
+  Wire.requestFrom(8, 2);
 
-  while (Wire.available())
-  {                       // peripheral may send less than requested
-    char c = Wire.read(); // receive a byte as character
-    Serial.print(c);      // print the character
+  if (Wire.available())
+  {
+    /*
+    Byte #1     Value     Event         Short     Decimal
+                0         Write         w         119
+                1         Press         p         112
+                2         Release       r         114
+                3         ReleaseAll    l         108
+    */
+    byte event = Wire.read();
+
+    /*
+    Refer to https://www.arduino.cc/reference/en/language/functions/usb/keyboard/keyboardmodifiers/
+      - Rest of ASCII is still the same
+        - eg. 65 = 'A'
+    */
+    byte decimal = Wire.read();
+
+    Keyboard.write(decimal);
+
+    // Debug
+    Serial.print(static_cast<char>(event));
+    Serial.print(" - ");
+    Serial.print(static_cast<char>(decimal));
+    Serial.println();
+
+    switch (event)
+    {
+    case 0: // w
+      write(decimal);
+      break;
+    case 1: // p
+      press(decimal);
+      break;
+    case 2: // r
+      release(decimal);
+      break;
+    case 3: // l
+      releaseAll();
+      break;
+    }
   }
 
-  delay(500);
+  delay(10);
+}
+
+void write(byte decimal)
+{
+  Keyboard.write(decimal);
+}
+
+void press(byte decimal)
+{
+  Keyboard.press(decimal);
+}
+
+void release(byte decimal)
+{
+  Keyboard.release(decimal);
+}
+
+void releaseAll()
+{
+  Keyboard.releaseAll();
 }
