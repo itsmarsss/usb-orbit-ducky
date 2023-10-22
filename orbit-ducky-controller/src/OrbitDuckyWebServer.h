92f0a695f9ee75b43cc144713f79953e855b39f5
itsmarsss <itsmarzzzzzz@gmail.com>
2023-10-22 16:55:49 -0400

    Disable root file editing

--- orbit-ducky-controller/src/OrbitDuckyWebServer.h
+++ orbit-ducky-controller/src/OrbitDuckyWebServer.h
@@ -41,89 +41,91 @@ void setupEndPoints()
 
   server.on("/api/new_script", HTTP_GET, [](AsyncWebServerRequest *request)
             {
-    if (request->hasParam("file_name"))
-    {
-      String file_name = hexToString(request->getParam("file_name")->value());
+    if (request -> hasParam("file_name")) {
+      String file_name = hexToString(request -> getParam("file_name") -> value());
 
       file_name.trim();
 
-      request->send(200, "text/html", newScript(file_name));
+      if (file_name.equals("/")) {
+        request -> send(200, "text/html", "Root file cannot be edited.");
+        return;
+      }
+
+      request -> send(200, "text/html", newScript(file_name));
     }
-    request->send(200, "text/html", "No 'file_name' provided."); });
+    request -> send(200, "text/html", "No 'file_name' provided."); });
 
   server.on("/api/delete_script", HTTP_GET, [](AsyncWebServerRequest *request)
             {
-    if (request->hasParam("file_name"))
-    {
-      String file_name = hexToString(request->getParam("file_name")->value());
+    if (request -> hasParam("file_name")) {
+      String file_name = hexToString(request -> getParam("file_name") -> value());
 
-      request->send(200, "text/html", deleteScript(file_name));
+      request -> send(200, "text/html", deleteScript(file_name));
     }
-    request->send(200, "text/html", "No 'file_name' provided."); });
+    request -> send(200, "text/html", "No 'file_name' provided."); });
 
   server.on("/api/get_script", HTTP_GET, [](AsyncWebServerRequest *request)
             {
-    if (request->hasParam("file_name"))
-    {
-      String file_name = hexToString(request->getParam("file_name")->value());
+    if (request -> hasParam("file_name")) {
+      String file_name = hexToString(request -> getParam("file_name") -> value());
 
-      request->send(200, "text/html", getScript(file_name));
+      request -> send(200, "text/html", getScript(file_name));
     }
-    request->send(200, "text/html", "No 'file_name' provided."); });
+    request -> send(200, "text/html", "No 'file_name' provided."); });
 
   server.on("/api/save_script", HTTP_GET, [](AsyncWebServerRequest *request)
             {
-    if (!(request->hasParam("file_name")))
-    {
-      request->send(200, "text/html", "No 'file_name' provided.");
+    if (!(request -> hasParam("file_name"))) {
+      request -> send(200, "text/html", "No 'file_name' provided.");
       return;
     }
-    if (!(request->hasParam("script")))
-    {
-      request->send(200, "text/html", "No 'script' provided.");
+    if (!(request -> hasParam("script"))) {
+      request -> send(200, "text/html", "No 'script' provided.");
       return;
     }
 
-    String file_name = hexToString(request->getParam("file_name")->value());
+    String file_name = hexToString(request -> getParam("file_name") -> value());
 
     file_name.trim();
 
-    String script = hexToString(request->getParam("script")->value());
-    request->send(200, "text/html", saveScript(file_name, script)); });
-
-  server.on("/api/run_script", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
-    if (request->hasParam("file_name"))
-    {
-      String file_name = hexToString(request->getParam("file_name")->value());
-
-      request->send(200, "text/html", runScript(file_name));
-    }
-    request->send(200, "text/html", "No 'file_name' provided."); });
-
-  server.on("/api/download_script", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
-    if (!(request->hasParam("file_name")))
-    {
-      request->send(200, "text/html", "No 'file_name' provided.");
+    if (file_name.equals("/")) {
+      request -> send(200, "text/html", "Root file cannot be edited.");
       return;
     }
 
-    String file_name = hexToString(request->getParam("file_name")->value());
+    String script = hexToString(request -> getParam("script") -> value());
+    request -> send(200, "text/html", saveScript(file_name, script)); });
 
-    if (!SPIFFS.exists("/" + file_name))
-    {
-      request->send(200, "text/html", "File with name '" + file_name + "' doesn't exists.");
+  server.on("/api/run_script", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+    if (request -> hasParam("file_name")) {
+      String file_name = hexToString(request -> getParam("file_name") -> value());
+
+      request -> send(200, "text/html", runScript(file_name));
+    }
+    request -> send(200, "text/html", "No 'file_name' provided."); });
+
+  server.on("/api/download_script", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+    if (!(request -> hasParam("file_name"))) {
+      request -> send(200, "text/html", "No 'file_name' provided.");
+      return;
+    }
+
+    String file_name = hexToString(request -> getParam("file_name") -> value());
+
+    if (!SPIFFS.exists("/" + file_name)) {
+      request -> send(200, "text/html", "File with name '" + file_name + "' doesn't exists.");
       return;
     }
 
     File download = SPIFFS.open("/" + file_name, FILE_READ);
 
-    AsyncWebServerResponse *response = request->beginResponse(200, "text/html", download.readString());
+    AsyncWebServerResponse * response = request -> beginResponse(200, "text/html", download.readString());
 
-    response->addHeader("Content-Type", "text/text");
-    response->addHeader("Content-Disposition", "attachment; filename=" + file_name + ".ods");
+    response -> addHeader("Content-Type", "text/text");
+    response -> addHeader("Content-Disposition", "attachment; filename=" + file_name + ".ods");
     download.close();
 
-    request->send(response); });
+    request -> send(response); });
 }
